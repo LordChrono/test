fb3f1593f0c6a17cd23a9aa75d0605d91a761091
Yury Bazhenov <bazhenov@trend.tech>
2025-08-08 12:53:03 +0200

    checkpoint arrays

---
+++ index.js
@@ -0,0 +1,23 @@
+const filterByAge = () => {};
+
+const filterByScore = () => {};
+
+const filterByServiceRecord = () => {};
+
+const sortByScore = () => {};
+
+const getMeanAge = () => {};
+
+const getMeanAgeReduce = () => {};
+
+const getNamesOnly = () => {};
+
+module.exports = {
+  filterByAge,
+  filterByScore,
+  filterByServiceRecord,
+  sortByScore,
+  getMeanAge,
+  getMeanAgeReduce,
+  getNamesOnly,
+};
